63005fcf89f434805fee7d79ce544b89d39b0be5
Sandra Alvarez <sandruskiag@gmail.com>
2019-05-15 11:32:30 +0200

    Create 2 portals and a bullet

---
+++ AR-Vuforia-Unity/Assets/Scripts/BulletMover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMover : MonoBehaviour
+{
+    #region PUBLIC_VARIABLES
+    public float speed = 5.0f;
+    public float lifeTime = 5.0f;
+
+    [HideInInspector]
+    public Vector3 direction = Vector3.zero;
+    #endregion
+
+    #region PRIVATE_VARIABLES
+    private float timer = 0.0f;
+    #endregion
+
+    void Update()
+    {
+        if (timer >= lifeTime)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        gameObject.transform.position += direction * speed * Time.deltaTime;
+
+        timer += Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collider collider)
+    {
+        if (LayerMask.LayerToName(collider.gameObject.layer) == "Obstacle")
+        {
+            // Destroy the bullet
+            Destroy(collider.gameObject);
+        }
+    }
+}
---
+++ AR-Vuforia-Unity/Assets/Scripts/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject bulletPrefab = null;
+    public GameObject otherPortal = null;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (LayerMask.LayerToName(collider.gameObject.layer) == "Bullet")
+        {
+            // Teleport the bullet the other portal
+            Destroy(collider.gameObject);
+
+            GameObject bullet = Instantiate(bulletPrefab, otherPortal.transform.position, Quaternion.identity, transform.parent);
+            BulletMover bulletMover = bullet.GetComponent<BulletMover>();
+            bulletMover.direction = otherPortal.transform.forward;
+        }
+    }
+}
