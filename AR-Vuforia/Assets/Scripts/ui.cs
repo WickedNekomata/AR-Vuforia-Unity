bd274faa8c1b52fc818a610e3a5c321d723a0b60
DatBeQuiet <kayter72@gmail.com>
2019-05-22 00:59:49 +0200

    play button

---
+++ AR-Vuforia/Assets/Scripts/ui.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ui : MonoBehaviour
+{
+    public GameObject playgos;
+
+    public void OnClickPlay()
+    {
+        playgos.SetActive(!playgos.activeSelf);
+        gameObject.SetActive(false);
+    }
+}
