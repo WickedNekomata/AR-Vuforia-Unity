b548f39b04c03c20d729a70fb73137df35b492cd
DatBeQuiet <kayter72@gmail.com>
2019-05-21 23:40:57 +0200

    contact point reflect

--- AR-Vuforia/Assets/ballPong.cs
+++ AR-Vuforia/Assets/ballPong.cs
@@ -14,28 +14,30 @@ public class ballPong : MonoBehaviour
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        transform.position = new Vector3(0.0f, transform.position.y, 0.0f);
-        rb.AddForce(new Vector3(Random.Range(0.0f, 1.0f), 0.0f, Random.Range(0.0f, 1.0f) * constantSpeed), ForceMode.Impulse);
-
         speed = constantSpeed;
     }
     
     void Update()
     {
-        rb.velocity = rb.velocity.normalized * speed;
-
-        Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
-        rb.velocity = transform.TransformDirection(localVelocity);
+        transform.position += transform.forward * speed;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer.Equals("Player"))
+        {
             speed += increaseOverBound;
+            Vector3 reflection = Vector3.Reflect(transform.forward, transform.InverseTransformDirection(collision.GetContact(0).normal));
+            transform.rotation = Quaternion.LookRotation(reflection);
+        }
+        else if (collision.gameObject.layer.Equals("Wall"))
+        {
+            Vector3 reflection = Vector3.Reflect(transform.forward, transform.InverseTransformDirection(collision.GetContact(0).normal));
+            transform.rotation = Quaternion.LookRotation(reflection);
+        }
         else if (collision.gameObject.layer.Equals("Goal"))
         {
-            transform.position = new Vector3(0.0f,0.0f,0.0f);
+            transform.position = new Vector3(0.0f, 0.0f, 0.0f);
             rb.AddForce(new Vector3(Random.Range(0.0f, 1.0f), 0.0f, Random.Range(0.0f, 1.0f) * constantSpeed), ForceMode.Impulse); speed = constantSpeed;
         }
     }
