ad94db1ca327a5f609788cc4cdc00fbc95661ef1
Sandra Alvarez <sandruskiag@gmail.com>
2019-05-21 12:55:24 +0200

    Add Vuforia

---
+++ AR-Vuforia/Assets/Scripts/BulletMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMover : MonoBehaviour
+{
+    #region PUBLIC_VARIABLES
+    public float speed = 5.0f;
+    public float lifeTime = 5.0f;
+    #endregion
+
+    #region PRIVATE_VARIABLES
+    private float timer = 0.0f;
+    #endregion
+
+    void Update()
+    {
+        if (timer >= lifeTime)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        gameObject.transform.position += transform.forward * speed * Time.deltaTime;
+
+        timer += Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Obstacle")
+        {
+            // Destroy the bullet
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ AR-Vuforia/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float angularVelocity = 1000.0f;
+
+    public GameObject map = null;
+    public GameObject bulletPrefab = null;
+
+    void Update()
+    {
+        // Instantiate bullet
+        if (Input.GetKeyDown(KeyCode.Space))
+            Instantiate(bulletPrefab, transform.position, transform.rotation, map.transform);
+
+        // Move turret
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.rotation *= Quaternion.AngleAxis(angularVelocity * Time.deltaTime, Vector3.up);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            transform.rotation *= Quaternion.AngleAxis(angularVelocity * Time.deltaTime, -Vector3.up);
+    }
+}
